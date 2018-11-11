8594c76f2be9a626d64b82a4cf76067f94f5b194
Daniel Zawadzki <hello@danielzawadzki.com>
2018-11-11 23:54:21 +0100

    Added basic styles, removed unnecessary boilerplate files and moved changed project structure.
    In details, basic styles contains colors, typography, minor stuff (like default box-shadow and transition) and all base elements. App component with all related files has been moved to the separated folder.

---
+++ src/containers/App/App.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+	render() {
+		return <div id="App">Test</div>;
+	}
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import 'normalize.css';
+import './assets/styles/base.scss';
+import App from './containers/App/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
